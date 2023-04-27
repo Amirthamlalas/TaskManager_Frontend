e0f86049422dd65dc48a40cb0fb4f51e4aa91e70
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2023-04-27 10:43:33 +0530

    added serviceapi

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -9,12 +11,22 @@ export class LoginPageComponent {
 
   email=""
   password=""
-constructor(){}
+  constructor(private api:ApiService,private route:Router){}
   login=()=>{
     let data:any={"email":this.email,"password":this.password}
 
-   
+    this.api.userLogin(data).subscribe(
+      (response:any)=>{
+        if(response.status=="success"){
+          alert("Login successfull")
+        }
+        else{
+          alert("Email or Password is incorrect")
+        }
+      }
+    )
     console.log(data)
   }
+
 
 }
