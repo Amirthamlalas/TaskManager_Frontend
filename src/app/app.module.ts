42b0c255411101e6cf37e2482c0861ee0030954b
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2023-04-27 09:12:13 +0530

    added email and password

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { ROUTES, RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
+
+const myroute :Routes=[
+  {
+    path:"",
+    component:LoginPageComponent
+  }
+
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -12,7 +22,9 @@ import { LoginPageComponent } from './login-page/login-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
