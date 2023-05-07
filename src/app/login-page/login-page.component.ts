ba8f8fad98d4eb92b0081312828a1b9717b2d7a5
Amirtha M Lalas <Amirtha@NTP-LAP-746.chn.nestgroup.net>
2023-05-07 17:33:32 +0530

    added validation

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -3,7 +3,7 @@ import { ApiService } from '../api.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { DatePipe } from '@angular/common';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -13,13 +13,36 @@ import { NgForm } from '@angular/forms';
 export class LoginPageComponent {
 
   email=""
+  emailInvalid: boolean = false;
+  emailRequired: boolean = false;
+  emailInvalidFormat: boolean = false;
+
+  onEmailChange() {
+    this.emailRequired = this.email == null || this.email.trim() === '';
+    this.emailInvalidFormat = !this.emailRequired && !this.email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+    this.emailInvalid = this.emailRequired || this.emailInvalidFormat;
+  } 
+ 
   password=""
+  passwordInvalid: boolean = false;
+  passwordRequired: boolean = false;
+  passwordMinLength: boolean = false;
+
+  onPasswordChange() {
+    this.passwordRequired = this.password == null || this.password.trim() === '';
+    this.passwordMinLength = !this.passwordRequired && this.password.length < 8;
+    this.passwordInvalid = this.passwordRequired || this.passwordMinLength;
+  }
+ 
   logoutTime:any={}
   
   constructor(private api:ApiService,private route:Router,private toastr:ToastrService,private datePipe: DatePipe){
    
   }
+  
+ 
 
+  
 
 login=()=>{
     let data:any={"email":this.email,"password":this.password}
